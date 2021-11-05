d0c2e2525563d55905eea1993c4d4cf16d6cb01e
km3gna <91228391+km3gna@users.noreply.github.com>
2021-11-05 11:49:15 -0400

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# Calculator
+
+def add(a,b):
+    result = a + b
+    return result
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return a / b
+
+def average(a,b):
+    return add(a,b) / 2
