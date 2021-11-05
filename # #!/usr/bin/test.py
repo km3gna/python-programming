bf0bbd33c019412e0c2264410eecb924cd3d364e
km3gna <91228391+km3gna@users.noreply.github.com>
2021-11-05 11:46:49 -0400

    Create test.py

---
+++ # #!/usr/bin/test.py
@@ -0,0 +1,16 @@
+# #!/usr/bin/python
+
+import sys
+import calculator
+
+# print(sys.argv) # take out
+
+if sys.argv[1].isdecimal() and sys.argv[2].isdecimal():
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+else:
+    print("Both must be integer")
+    sys.exit()
+
+print("Sum of arguments:", calculator.add(num1,num2))
+print("Average of arguments:", calculator.average(num1,num2))
